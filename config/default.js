dd72ecedecc3bc116746e8e97477fa48b203b9de
Jason Sebring <mail@jasonsebring.com>
2015-06-16 07:29:16 -0700

    skeleton update

---
+++ config/default.js
@@ -0,0 +1,6 @@
+var config = {
+	port: 3000,
+	mongo: 'mongodb://localhost:27017/kitgui'
+};
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var config = require('config');
+var port = config.port || 3000;
+var cors = require('cors');
+
+app.use(cors());
+app.use('/api/v1', require('./lib/api/v1'));
+
+var server = app.listen(config.port, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('KitGUI listening at %s:%s', host, port);
+});
