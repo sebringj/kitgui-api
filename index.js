309672972954dee1fe98a18f7573c95ff2f54c7a
Jason Sebring <mail@jasonsebring.com>
2015-08-01 08:36:50 -0700

    added isomorphic react

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ var port = config.port || 3000;
 var cors = require('cors');
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 app.use(bodyParser.json());
-app.use(cors());
-app.use('/api/v1', require('./lib/api/v1'));
+app.use('/api/v1', cors(), require('./lib/api/v1'));
+app.use('/', require('./lib/web/controllers'));
 
 var server = app.listen(config.port, function() {
 	var host = server.address().address;
 	var port = server.address().port;
-	console.log('KitGUI listening at %s:%s', host, port);
+	console.log('KitGUI API listening at %s:%s', host, port);
 });
---
+++ lib/web/views/index.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Index = React.createClass({
+  render: function() {
+    return (
+      <div>testing 123</div>
+    );
+  }
+});
+
+module.exports = Index;
